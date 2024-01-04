39e3716f0b276d1f76c19bdfdb86abb7ff671d4d
AngieVaMo <anvamopa2@gmail.com>
2024-01-04 18:13:10 -0500

    Done!

--- src/Components/Tickets.js
+++ src/Components/Tickets.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import './Tickets.css';
+import React from "react";
+import "./Tickets.css";
 
-export const Tickets = ({ send, context }) => {
+export const Tickets = ({ send, state }) => {
   const finish = () => {
-    send('FINISH')
+    send({ type: "FINISH" });
   };
 
+  const { selectedCountry } = state.context;
+
   return (
-    <div className='Tickets'>
-      <p className='Tickets-description description'>Gracias por volar con book a fly 💚</p>
-      <div className='Tickets-ticket'>
-        <div className='Tickets-country'>Colombia</div>
-        <div className='Tickets-passengers'>
+    <div className="Tickets">
+      <p className="Tickets-description description">
+        Gracias por volar con book a fly 💚
+      </p>
+      <div className="Tickets-ticket">
+        <div className="Tickets-country">{selectedCountry}</div>
+        <div className="Tickets-passengers">
           <span>✈</span>
+          {state.context.passengers.map((person, idx) => {
+            return <p key={idx}>{person}</p>;
+          })}
         </div>
       </div>
-      <button onClick={finish} className='Tickets-finalizar button'>Finalizar</button>
+      <button onClick={finish} className="Tickets-finalizar button">
+        Finalizar
+      </button>
     </div>
   );
-}; 
+};
--- src/Machines/bookingMachine.js
+++ src/Machines/bookingMachine.js
@@ -1,32 +1,122 @@
-import { createMachine } from "xstate";
+import { assign, createMachine, fromPromise } from "xstate";
+import { fetchCountries } from "../Utils/api";
 
-const bookingMachine = createMachine({
-  id: "buy plane tickets",
-  initial: "initial",
+const fillCountries = {
+  initial: "loading",
   states: {
-    initial: {
-      on: {
-        START: "search",
+    loading: {
+      invoke: {
+        id: "getCountries",
+        src: fromPromise(() => fetchCountries()),
+        onDone: {
+          target: "success",
+          actions: "setCountries",
+        },
+        onError: {
+          target: "failure",
+          actions: assign({
+            error: "Falló el request",
+          }),
+        },
       },
     },
-    search: {
+    success: {},
+    failure: {
       on: {
-        CONTINUE: "passengers",
-        CANCEL: "initial",
-      },
-    },
-    tickets: {
-      on: {
-        FINISH: "initial",
-      },
-    },
-    passengers: {
-      on: {
-        DONE: "tickets",
-        CANCEL: "initial",
+        RETRY: { target: "loading" },
       },
     },
   },
-});
+};
+
+const bookingMachine = createMachine(
+  {
+    id: "buy plane tickets",
+    initial: "initial",
+    context: {
+      passengers: [],
+      selectedCountry: "",
+      countries: [],
+      error: "",
+    },
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: "search",
+          },
+        },
+      },
+      search: {
+        on: {
+          CONTINUE: {
+            target: "passengers",
+            actions: assign({
+              selectedCountry: ({ event }) => event.value,
+            }),
+          },
+          CANCEL: "initial",
+        },
+        ...fillCountries,
+      },
+      tickets: {
+        after: {
+          5000: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+        },
+        on: {
+          FINISH: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+        },
+      },
+      passengers: {
+        on: {
+          DONE: {
+            guard: "moreThanOnePassenger",
+            target: "tickets",
+          },
+          CANCEL: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+          ADD: {
+            target: "passengers",
+            actions: assign(({ context, event }) =>
+              context.passengers.push(event.value)
+            ),
+          },
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      imprimirInicio: () => console.log("Imprimir Inicio"),
+      imprimirEntrada: () => console.log("Imprimir Entrada"),
+      imprimirSalida: () => console.log("Imprimir Salida"),
+      cleanContext: ({ context }) => {
+        context.passengers = [];
+        context.selectedCountry = "";
+        return context;
+      },
+      // assign({
+      //   selectedCountry: "",
+      //   passengers: [],
+      // }),
+      setCountries: assign({
+        countries: ({ event }) => event.output,
+      }),
+    },
+    guards: {
+      moreThanOnePassenger: ({ context }) => {
+        return context.passengers.length > 0;
+      },
+    },
+  }
+);
 
 export default bookingMachine;
